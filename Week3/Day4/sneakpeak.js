daa76200dbedf7417d3eae73ed163f3f60ffdb2f
JBama023 <jbama023@gmail.com>
2021-08-19 14:10:38 -0500

    Python Sneak Peek

---
+++ Week3/Day4/sneakpeak.js
@@ -0,0 +1,36 @@
+// In Python
+// listOfPeoplesNames = ["Joe", "sara"]
+
+// In JavaScript
+const listOfPeoplesNames = ["Joe", "Sara"]
+number1 = "2"
+// const means constant, it can't be reassigned
+
+// let can be reassigned
+
+let zak = "Zak"
+// Dont use var
+
+// functions
+// In Python:
+// def functionName():
+//     indent
+//     return
+
+// in JavaScript:
+function functionName(){
+    //body of function
+    return
+    // as long as you're inside curly braces,
+    // you will be inside the body of the function
+}
+// we invoke a function the same as Python, functionname()
+//method 2 // ES6, the more modern way to write a function
+const functionName2 = () =>{}
+
+() => {
+    return "Joe is super cool"
+}
+
+const array = [1,2,3,4,5]
+
