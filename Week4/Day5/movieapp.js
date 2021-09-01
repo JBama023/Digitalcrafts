f4e066e9ab9ff90f27c5a483c801a021177620b4
JBama023 <jbama023@gmail.com>
2021-08-31 23:12:50 -0500

    able to get movie app fully working(finally)

--- Week4/Day5/movieapp.js
+++ Week4/Day5/movieapp.js
@@ -9,20 +9,20 @@ const findMovies = document.querySelector(".movie-button");
 async function getMovies() {
     movies.innerHTML = "";
     const userInput = document.querySelector(".input");
-    const getMovies = await fetch(movieURL + `&s=${userInput}`);
+    const getMovies = await fetch(movieURL + `&s=${userInput.value}`);
     const jsonMovies = await getMovies.json();
     console.log(jsonMovies)
     
     
-    for (const movie of jsonMovies) {
+    for (const movie of jsonMovies.Search) {
         const moviePoster = document.createElement("div");
         moviePoster.className = "posters";
         const movieNames = document.createElement("h2");
         const moviePictures = document.createElement("img");
         movieNames.innerHTML = movie.Title;
         moviePictures.src = movie.Poster;
-        moviePictures.style.height = "200px";
-        moviePictures.style.width = "200px";
+        moviePictures.style.height = "400px";
+        moviePictures.style.width = "400px";
         moviePoster.append(movieNames, moviePictures);
         movies.append(moviePoster);
     }
